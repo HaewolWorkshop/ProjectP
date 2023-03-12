9458902654cfeb939a975e702189430aac005eb0
Kobins <34197983+Kobins@users.noreply.github.com>
2023-03-12 19:21:50 +0900

    Feat : 브래킬 이동 임시 구현

--- Assets/Scripts/Enemy/Brakill/Brakill.cs
+++ Assets/Scripts/Enemy/Brakill/Brakill.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace HaewolWorkshop
@@ -15,13 +16,60 @@ namespace HaewolWorkshop
         
         [field: SerializeField] public EnemyWanderType WanderType { get; set; }
         [field: SerializeField] public EnemyWanderSpots WanderSpots { get; set; }
+
+
+        private float initialBaseOffset;
         
         protected override void Awake()
         {
             base.Awake();
+
+            initialBaseOffset = Agent.baseOffset;
             
             // SetUp(DummyEnemyState.Idle); // TODO
             SetUp(BrakillStates.Wander);
         }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            
+            AdjustBaseOffsetByCeil();
+        }
+
+        // 머리위로 Raycast, 일정 수준 이상 내려오면 BaseOffset 조정
+        private void AdjustBaseOffsetByCeil()
+        {
+            if (initialBaseOffset <= 0f)
+            {
+                return;
+            }
+
+            var position = transform.position;
+            float newOffset;
+            // 위쪽으로 raycast
+            if (Physics.Raycast(position, Vector3.up, out var hitInfo, Data.MinimumDistanceToCeil, Data.CeilMask.value))
+            {
+#if UNITY_EDITOR
+                Debug.DrawLine(position, hitInfo.point, Color.yellow);
+#endif
+                float distance = (hitInfo.point - position).magnitude;
+                // 최소거리 - 지형에 도달한 거리 == 충돌할 여지가 있는 거리
+                // 이를 BaseOffset에서 빼 줌
+                newOffset = Mathf.Max(0f,  initialBaseOffset - (Data.MinimumDistanceToCeil - distance));
+            }
+            else
+            {
+#if UNITY_EDITOR
+                Debug.DrawRay(position, Vector3.up * Data.MinimumDistanceToCeil, Color.white);
+#endif
+                // 아니면 기본 base offset으로 설정
+                newOffset = initialBaseOffset;
+            }
+
+            // 적절하게 보간 (FixedUpdate라 약간 끊겨보임)
+            Agent.baseOffset = Mathf.Lerp(Agent.baseOffset, newOffset, Data.CeilAdjustSpeed * Time.deltaTime);
+        }
+
     }
 }
--- Assets/Scripts/Enemy/Brakill/BrakillData.cs
+++ Assets/Scripts/Enemy/Brakill/BrakillData.cs
@@ -5,6 +5,8 @@ namespace HaewolWorkshop
     [CreateAssetMenu(fileName = "Brakill Data", menuName = "Data/Enemy Data/Brakill Data", order = 0)]
     public class BrakillData : EnemyData
     {
-        
+        [field: SerializeField] public float MinimumDistanceToCeil { get; private set; } = 10f;
+        [field: SerializeField] public float CeilAdjustSpeed { get; private set; } = 10f;
+        [field: SerializeField] public LayerMask CeilMask { get; private set; }
     }
 }
