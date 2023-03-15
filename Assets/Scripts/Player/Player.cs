84a6dff2947519360d0eabb0e06171c94cf823b5
rbase0218 <rbase@naver.com>
2023-03-15 16:46:34 +0900

    Chore : Climb State 관련 스크립트 삭제

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -19,7 +19,6 @@ namespace HaewolWorkshop
             NormalJump,
             NormalSprint,
             NormalStealth,
-            NormalClimb
             // ETC
             
             // Henshin 
@@ -30,30 +29,10 @@ namespace HaewolWorkshop
 
         public float moveSpeed = .0f;
 
-        [Header("Climb")] 
-        [SerializeField] public float _wallAngleMax;
-        [SerializeField] public float _groundAngleMax;
-        [SerializeField] public LayerMask _layerMaskClimb;
-
-        [SerializeField] public float _overpassHeight;
-        [SerializeField] public float _stepHeight;
-
-        [SerializeField] public Vector3 _endOffset;
-        [SerializeField] public Vector3 _climbOriginDown;
-
-        [SerializeField] public CapsuleCollider collider;
-
-        public Vector3 endPosition;
-        public RaycastHit downRaycastHit;
-        public RaycastHit forwardRaycastHit;
-
-        public bool _climbing;
-        
         private void Awake()
         {
             rigidbody = GetComponent<Rigidbody>();
             animator = GetComponent<Animator>();
-            collider = GetComponent<CapsuleCollider>();
             
             InitInputs();
             
@@ -71,32 +50,6 @@ namespace HaewolWorkshop
         {
             base.FixedUpdate();
         }
-
-        private void OnDrawGizmos()
-        {
-            if (GetComponent<CapsuleCollider>() == null)
-                return;
-            
-            DrawWorld();
-        }
-
-        private void DrawWorld()
-        {
-            float heightScale = Mathf.Abs(transform.lossyScale.y);
-            float radiusScale = Mathf.Max(Mathf.Abs(transform.lossyScale.x), Mathf.Abs(transform.lossyScale.z));
-
-            float radius = collider.radius * radiusScale;
-            float totalHeight = Mathf.Max(collider.height * heightScale, radius * 2);
-
-            Vector3 direction = transform.up;
-            Vector3 center = transform.TransformPoint(collider.center);
-            Vector3 top = center + direction * (totalHeight / 2 - radius);
-            Vector3 bottom = center - direction * (totalHeight / 2 - radius);
-            
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(top, radius);
-            Gizmos.DrawWireSphere(bottom, radius);
-
-        }
+     
     }
 }
--- Assets/Scripts/Player/State/Normal/PlayerNormalMoveState.cs
+++ Assets/Scripts/Player/State/Normal/PlayerNormalMoveState.cs
@@ -1,42 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace HaewolWorkshop
 {
     [FSMState((int) Player.States.NormalMove)]
     public class PlayerNormalMoveState : PlayerMoveState
     {
-        #region OwnerInterface
-
         public PlayerNormalMoveState(IFSMEntity owner) : base(owner)
         {
         }
 
-        #endregion
-
         private readonly int animXParam = Animator.StringToHash("InputXParam");
         private readonly int animZParam = Animator.StringToHash("InputZParam");
-
-        public override void InitializeState()
-        {
-            base.InitializeState();
-            
-        }
 
         public override void FixedUpdateState()
         {
             base.FixedUpdateState();
-            
-            ownerEntity.animator.SetFloat(animXParam, Mathf.Round(moveInput.x * 100f) / 100f);
-            ownerEntity.animator.SetFloat(animZParam, Mathf.Round(moveInput.y * 100f) / 100f);
-        }
 
-        public override void ClearState()
-        {
-            base.ClearState();
+            ownerEntity.animator.SetFloat(animXParam, moveInput.x);
+            ownerEntity.animator.SetFloat(animZParam, moveInput.y);
         }
-
     }
 }
