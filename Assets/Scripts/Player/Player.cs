578cdc6dcdfaf35700caac21003abd96d95c5dd3
rbase0218 <rbase@naver.com>
2023-03-15 17:31:27 +0900

    Fix : 필요없는 using 삭제

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
-using Unity.VisualScripting;
 using UnityEngine;
-using Vector3 = UnityEngine.Vector3;
 
 namespace HaewolWorkshop
 {
